ef0fb66f0f404454e5ccd8e00f6e311cfe38ac5b
iaguilar <iaguilaror@gmail.com>
2022-09-14 08:49:33 -0500

    add mem usage

---
+++ scripts/02-2_ram_usage_per_user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# hostname
+elhost=$(hostname)
+
+# set time
+timestamp=$1
+
+# run smem on all users
+smem -u -p \
+| tr -d "%" \
+| tr -s " " \
+| awk \
+	-v stamp="$timestamp" \
+	-v host="$elhost" \
+	'
+	BEGIN { FS=OFS=" "}
+	{print stamp, $1, $5, host}
+	' \
+| tail -n+2 \
+| tr " " "\t"
