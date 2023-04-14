498e506a631f3e39fea16a6b12bdf564259c231a
iaguilar <iaguilaror@gmail.com>
2023-04-13 23:38:04 -0600

    update scripts

--- scripts/02_number_of_processes.sh
+++ scripts/02_number_of_processes.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Define a limit for connections
-min_non_offending_processes=5        # to exclude user with less than these processesç
-#max_allowed_processes=10 ## default 5
-
 # hostname
 elhost=$(hostname)
-
-# set time
-timestamp=$1
 
 # find users connected
 ps -aux	\
@@ -21,12 +14,8 @@ ps -aux	\
 | tr -s " " \
 | sed -s "s# ##" \
 | sort -nr \
-| awk \
-	-v low_limit="$min_non_offending_processes" \
-	-v stamp="$timestamp" \
-	-v host="$elhost" \
-	'
+| awk -v host="$elhost" \
+'
 	BEGIN{ FS=OFS=" " }
-	$1 > low_limit { print stamp, $0, host }
-	' \
-| tr " " "\t"
+	{ print $2, $1, host }
+'
