a156f0c2da90bb7d087305c4c273c645c5900e09
iaguilar <iaguilaror@gmail.com>
2023-04-30 00:36:02 -0600

    change order in table of RMD

--- scripts/B_runthermd.R
+++ scripts/B_runthermd.R
@@ -9,12 +9,16 @@ args = commandArgs( trailingOnly = TRUE )
 
 ## Passing args to named objects
 rmd_file <- args[1]
+## Create the a custom output name for the rmd
+output_pdf <- paste0( Sys.Date(),"_uso_inadecuado_nodos", ".pdf") 
+
 
 # define the localdir to avoid errors during rendering
 localdir <- getwd( )
 
 # call the renderizer
 rmarkdown::render(  input = rmd_file,
+                    output_file = output_pdf,
                     output_dir = localdir, # if we dont fix the wd here, knit will fail when NF tries to execute it from a different workdir
                     intermediates_dir = localdir, # if we dont fix the wd here, knit will fail when NF tries to execute it from a different workdir
                     knit_root_dir = localdir ) # if we dont fix the wd here, knit will fail when NF tries to execute it from a different workdir
