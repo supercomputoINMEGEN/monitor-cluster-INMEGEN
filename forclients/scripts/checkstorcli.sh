bd8df9da2d001fb841bbaedd69da3858a9e7cf5b
Jefatura de Supercomputo <supercomputoinmegen@gmail.com>
2023-08-24 14:32:02 -0700

    update access

--- forclients/scripts/checkstorcli.sh
+++ forclients/scripts/checkstorcli.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# hostname
-elhost=$(hostname)
-
 # find lines for info
 line_for_VD=$( storcli64 /c0 show all | grep "VD LIST" -n | cut -d":" -f1 )
 line_for_PD=$( storcli64 /c0 show all | grep "PD LIST" -n | cut -d":" -f1 )
@@ -11,10 +8,9 @@ line_for_PD=$( storcli64 /c0 show all | grep "PD LIST" -n | cut -d":" -f1 )
 # echo "[DEBUG] PD line found at $line_for_PD"
 
 storcli64 /c0 show all \
-| sed -n '$line_for_VD,$line_for_PD p' \
+| sed -n "$line_for_VD,$line_for_PD p" \
 | grep -v "=" \
 | grep -v "^$" \
 | grep "^[0-9]" \
 | tr -s " " \
-| tr " " "_" \
-| sed 's#$_##'
+| tr " " "_"
