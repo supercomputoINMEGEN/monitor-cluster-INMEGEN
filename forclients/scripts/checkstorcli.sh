e6a3426c56b678d94f1cad02465c3e00b729c8af
israel aguilar <iaguilaror@gmail.com>
2023-08-24 15:38:25 -0600

    add test for not storcli systems

--- forclients/scripts/checkstorcli.sh
+++ forclients/scripts/checkstorcli.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# validate if this is a storcli system
+if ! command -v storcli64 &> /dev/null
+then
+    echo "NOT_A_storcli64_SYSTEM"
+    exit
+fi
+
 # find lines for info
 line_for_VD=$( storcli64 /c0 show all | grep "VD LIST" -n | cut -d":" -f1 )
 line_for_PD=$( storcli64 /c0 show all | grep "PD LIST" -n | cut -d":" -f1 )
