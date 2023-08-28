b50a6e83120cd8d180cfdb5f3244b5641e80643b
Israel Aguilar <iaguilaror@gmail.com>
2023-08-28 14:59:10 -0600

    include groups heatmap

--- foradmin/admin_scripts.R
+++ foradmin/admin_scripts.R
@@ -33,7 +33,47 @@ user_propagation.f <- function( the_data, the_user, the_hostnames ) {
     labs( title = "Propagacion de usuarios",           
           x = "Registered and Hostname",
           y = "User",           
-          fill = "UID test" ) +
+          fill = "Homo-UID test",
+          caption = "This test shows if a user has the same UID in every node" ) +
+    theme_minimal( base_size = 15 ) +
+    theme( axis.text.x = element_text( angle = 90, hjust = 0.5 ) )
+  
+}
+
+# Funcion for group heatmap
+group_propagation.f <- function( the_data, the_group, the_hostnames ) {
+  
+  # Filter data based on user selection
+  filtered_data <- subset( x = the_data,
+                           gp == the_group ) %>%
+    mutate( col_tag = ifelse( test = length( unique( pull( ., GID ) ) ) == 1,
+                             no = "fail", yes = "ok") )
+  
+  # mix filtered and defaults
+  mixed_data <- left_join( x = the_hostnames,
+                           y = filtered_data,
+                           by = "registered_and_hostname" ) %>%
+    mutate( gp = unique( na.omit( pull( ., gp ) ) ),
+            col_tag = ifelse( test = is.na( col_tag ),
+                              yes = "missing", no = col_tag ) )
+  
+  # Create the heatmap plot
+  ggplot( data = mixed_data,
+          mapping = aes( x = registered_and_hostname,
+                         y = gp,
+                         label = GID,
+                         fill = col_tag ) ) +
+    geom_tile( color = "black" ) +
+    geom_text( ) +
+    scale_x_discrete( limits = the_hostnames$registered_and_hostname ) +
+    scale_fill_manual( values = c( "fail" = "tomato",
+                                   "ok" = "skyblue",
+                                   "missing" = "white" ) ) +
+    labs( title = "Propagacion de Grupos",
+          x = "Registered and Hostname",
+          y = "Group",
+          fill = "Homo-GID test",
+          caption = "This test shows if a group has the same GID in every node" ) +
     theme_minimal( base_size = 15 ) +
     theme( axis.text.x = element_text( angle = 90, hjust = 0.5 ) )
   
--- foradmin/app.R
+++ foradmin/app.R
@@ -52,21 +52,33 @@ ui <- dashboardPage(
       tabItems(        
         
         # Primer tab conten; Usuarios
-        tabItem(          
+        tabItem(
           tabName = "users",
           fluidRow(            
-            box(title = "Buscar user",              
-                selectInput(inputId = "user_select",
-                            label = "Seleccionar",                          
-                            choices = NULL), 
-                width = 3),            
-            box(
-              title = "Propagacion del Usuario",              
-              plotOutput("heatmap"),
-              width = 10)
+            box( title = "Buscar USER",              
+                 selectInput( inputId = "user_select",
+                              label = "Seleccionar",                          
+                              choices = NULL ), 
+                 width = 3 ),            
+            box( title = "Propagacion del Usuario",              
+                 plotOutput( "heatmap_user" ),
+                 width = 10 )
+          ) # end fluidrow        
+        ),
+        tabItem(
+          tabName = "groups",
+          fluidRow(
+            box( title = "Buscar GROUP",              
+                 selectInput( inputId = "group_select",
+                              label = "Seleccionar",                          
+                              choices = NULL ), 
+                 width = 3 ),
+            box( title = "Propagacion del Grupo",              
+                 plotOutput( "heatmap_groups" ),     ## probar con otro objeto que no sea el heatmap solo para ver si lo renderea
+                 width = 10 )
           ) # end fluidrow        
         )
-      )
+      ) #End of  tabItemS( )
     )
   )
 )
@@ -114,24 +126,44 @@ server <- function(input, output, session) {
   
   # get all the possible hostnames
   allhostnames <- all_usuarios %>%     
-    select(registered_and_hostname) %>% 
-    unique() %>%     
-    mutate(default = "missing") %>% 
+    select( registered_and_hostname ) %>% 
+    unique( ) %>%     
+    mutate( default = "missing" ) %>% 
     arrange( registered_and_hostname )
   
   # Update user selection choices  
-  users <- unique(all_usuarios$user) %>% sort()
+  users <- unique( all_usuarios$user ) %>% sort( )
   
-  updateSelectInput(session, "user_select", choices = users)  
+  updateSelectInput( session = session, inputId = "user_select", choices = users )
   
-  # Render the heatmap  
-  output$heatmap <- renderPlot({
+  # Render the heatmap for users
+  output$heatmap_user <- renderPlot({
     
     user_propagation.f( the_data = all_usuarios,
                         the_user = input$user_select,
                         the_hostnames = allhostnames )
     
-  })  
+  })
+  
+  # Read the data  for groups
+  all_groups <- vroom( file = "logs/allgroups.tsv" )
+  
+  # get all the possible hostnames
+  allhostnames_2 <- allhostnames
+  
+  # Update groups selection choices  
+  groups <- unique( all_groups$gp ) %>% sort( )
+  
+  updateSelectInput( session = session, inputId = "group_select", choices = groups )
+  
+  # Render the heatmap for users
+  output$heatmap_groups <- renderPlot({
+    
+    group_propagation.f( the_data = all_groups,
+                         the_group = input$group_select,
+                         the_hostnames = allhostnames_2 )
+    
+  })
   
   # Mostrar el contenido del dashboard cuando el usuario ha iniciado sesión  
   observe({
